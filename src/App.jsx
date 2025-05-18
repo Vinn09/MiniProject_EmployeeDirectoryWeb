4fcd6463fdd351e1ea7b51e35bd9c2d8b6b31b04
Vinn09 <vinamandari1@gmail.com>
2025-05-18 11:34:47 +0800

    rename folder component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Register from './pages/register';
 import Login from './pages/login';
 import Home from "./pages/home";
 import DetailUser from './pages/detailUser';
-import ProtectedRoute from './component/ProtectedRoute';
+import ProtectedRoute from './components/protectedRoute';
 import LandingPage from './pages/landingPage';
 
 function App() {
